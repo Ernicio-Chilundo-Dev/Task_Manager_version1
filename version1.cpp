c37926ce858c15110516dc2b77ac207d8b61eef8
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-23 16:05:28 +0200

    but one

--- version1.cpp
+++ version1.cpp
@@ -12,7 +12,13 @@ int main()
     int alternativa;
     do
     {
-        /* code */
+        cout << "######################## Gerenciador de Tarefas ###########################\n";
+        cout << "1. Adicionar Tarefas\n";
+        cout << "2. Listar todas as tarefas\n";
+        cout << "3. Marcar a tarefa concluida\n";
+        cout << "4. Remover Tarefa\n";
+        cout << "5. Sair\n";
+        cout << "Informe a sua alternsativa: ";
     } while (alternativa != 5);
     return 0;
 }
