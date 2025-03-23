7470a46e8800b95e68a2e5202e0be7b0ea687066
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-23 15:59:12 +0200

    iniciado o projeto task_manaager  versao2 por que nele ta incluido a funcao de salvar um arquivo , carregar  e marcar a tarefa concluida

---
+++ version1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// Funcao principal
+int main()
+{
+    carregarTarefa();
+    int alternativa;
+    do
+    {
+        /* code */
+    } while (alternativa != 5);
+    return 0;
+}
